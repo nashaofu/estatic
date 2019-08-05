6331d2703a50b6b1cb3a356a80437554a7dc8cd9
nashaofu <diaocheng@outlook.com>
2019-08-05 23:15:57 +0800

    feat 编写命令行解析

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,77 @@
+import yargs from 'yargs'
 import express from 'express'
+import detect from 'detect-port'
 import serveIndex from 'serve-index'
 
-export default function a(root = '.') {
+interface Argv {
+  _: string[]
+  dir: string
+  port?: number
+  open?: boolean
+}
+
+async function serve(argv: Argv): Promise<any> {
+  function listen(port: number) {
+    return new Promise((resolve, reject) => {
+      try {
+        app.listen(port, () => {
+          resolve()
+        })
+      } catch (e) {
+        reject(e)
+        console.log(e)
+      }
+    })
+  }
   const app = express()
+  const root = argv.dir
 
   app.use(express.static(root), serveIndex(root, { icons: true, hidden: true }))
 
-  app.listen(8080)
+  const port = await detect(argv.port || 8080)
+  await listen(port)
+  console.log(port)
 }
+
+yargs
+  .strict(true)
+  .scriptName('estatic')
+  .usage('$0 [dir]')
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .wrap(null)
+  .fail((msg: string, err: Error) => {
+    yargs.showHelp()
+    console.log()
+    if (err) console.error(msg)
+    process.exit(1)
+  })
+  .command(
+    '$0 [dir]',
+    '启动静态文件服务器',
+    (yargs: yargs.Argv): void => {
+      yargs
+        .positional('dir', {
+          type: 'string',
+          default: '.',
+          describe: '静态文件夹目录'
+        })
+        .option('port', {
+          alias: 'p',
+          type: 'number',
+          default: 8080,
+          requiresArg: true,
+          describe: '设置服务器端口号'
+        })
+        .option('open', {
+          alias: 'o',
+          type: 'boolean',
+          default: false,
+          describe: '是否自动打开浏览器'
+        })
+    },
+    (argv: yargs.Arguments<Argv>) => {
+      console.log('dsds', argv)
+    }
+  )
+  .parse(process.argv.slice(2))
