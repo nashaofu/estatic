71947eb106499a82357068a9a52f91df6e34679c
nashaofu <diaocheng@outlook.com>
2022-06-01 22:20:11 +0800

    feat: 添加请求日志输出

--- src/serve.ts
+++ src/serve.ts
@@ -1,11 +1,12 @@
-import os from 'os';
-import open from 'open';
-import yargs from 'yargs';
 import colors from 'colors';
-import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import detect from 'detect-port';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import open from 'open';
+import os from 'os';
 import serveIndex from 'serve-index';
+import yargs from 'yargs';
 
 /**
  * 获取局域网ip
@@ -38,6 +39,8 @@ export default async (argv: yargs.Arguments<Options>): Promise<void> => {
   const app = express();
   const port = await detect(argv.port || 8080);
   const base = `/${argv.base.replace(/^\/+|\/+$/g, '')}`;
+
+  app.use(morgan('combined'));
 
   app.use(
     base,
